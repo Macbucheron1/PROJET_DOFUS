e1a6d69b31085e4e3a19d7d43c30866bc8a0ad2f
Macbucheron1 <95475157+Macbucheron1@users.noreply.github.com>
2022-05-13 11:14:28 +0200

    Update HEADER.h

--- HEADER.h
+++ HEADER.h
@@ -149,7 +149,7 @@ void init_acteur(t_acteur* acteur, int position_x, int position_y, BITMAP* skin,
 /* ----------- PRINCIPALE ----------- */
 
 void menu_principal(void); // Lance le menu principale
-void jouer(void); // Permet de jouer
+int jouer(void); // Permet de jouer
 void credit_en_cours(BITMAP* page, t_decor* visuel_menu, BITMAP* soldat, t_acteur mesActeurs[], int* delay, unsigned int* temps, BITMAP* tab_bitmap[]) ; //Lance les credits
 void parametre_en_cours(BITMAP* page, t_decor* visuel_menu, SAMPLE* musique, int* volume, BITMAP* soldat, t_acteur mesActeurs[], int* delay, BITMAP* tab_bitmap[], unsigned int* temps); // Lance les parametres
 void apercu_classe_en_cours(BITMAP* page, t_decor* visuel_menu, BITMAP* soldat, int* delay, t_acteur mesActeurs[], BITMAP* tab_bitmap[],unsigned int* temps); // Lance l'apercu des classes
