45125784f2c0dbec3a0d86a8c4ed8e9f053a8f32
Macbucheron1 <95475157+Macbucheron1@users.noreply.github.com>
2022-05-20 17:49:45 +0200

    Update ATTAQUE_TANK.c

--- ATTAQUE_TANK.c
+++ ATTAQUE_TANK.c
@@ -1,17 +1,15 @@
 #include "HEADER.h"
 
-/**  finie sans animation **/
+/**  finie sans animation **/ //rajouter verif bouclier
 
-void c_a_c_tank(t_joueur* tab_j,int i,BITMAP* buffer,t_map carte,int zoneAttaque[20][16],BITMAP* animation,int nb_joueur,BITMAP* soldat)
+void c_a_c_tank(t_joueur* tab_j,int i,BITMAP* buffer,t_map carte,int zoneAttaque[20][16],BITMAP* animation,int nb_joueur,BITMAP* soldat, int respiration)
 {
     /*attaque au corps a corps
     (i indice du joueur qui joue)*/
-
     int PA=5; //cout en point d'action
     int degat=420; //PV enlever a ladversaire
     int distance = 1;//distance d'attaque
-    int j=joueur_sur_case_ou_pas(carte,zoneAttaque,tab_j,buffer,animation,nb_joueur); //indice du joueur attaquÈ (4 si ce n'est personne)
-
+    int j=joueur_sur_case_ou_pas(carte,zoneAttaque,tab_j,buffer,animation,nb_joueur); //indice du joueur attaqu√© (4 si ce n'est personne)
     if (action_possible(tab_j,i,PA))
     {
         CalculAttaque_zone(buffer,carte,tab_j[i].position_colonne,tab_j[i].position_ligne,zoneAttaque,distance);
@@ -20,9 +18,9 @@ void c_a_c_tank(t_joueur* tab_j,int i,BITMAP* buffer,t_map carte,int zoneAttaque
         {
            if(!(tab_j[j].bouclier)) //si le joueur ne se protege pas avec un bouclier
             {
-                tab_j[j].pv_actuel-=degat; //retire les pv au joueur attaquÈ
+                tab_j[j].pv_actuel-=degat; //retire les pv au joueur attaqu√©
                 tab_j[i].pa_actuel-=PA; //retire les PA au joueur qui joue
-                affichage_degat_soin(tab_j,j,buffer,degat,soldat,carte,nb_joueur,1);
+                affichage_degat_soin(tab_j,j,buffer,degat,soldat,carte,nb_joueur,1, respiration);
             }
             else
             {
@@ -37,20 +35,18 @@ void c_a_c_tank(t_joueur* tab_j,int i,BITMAP* buffer,t_map carte,int zoneAttaque
     }
 }
 
-/**  finie sans animation **/
-
-void roulement(t_joueur* tab_j,int i,BITMAP* page,int *quelle_attaque,t_map carte,int zoneAttaque[20][16],BITMAP* animation,int nb_joueur,BITMAP* soldat)
+/** pas finie **/ //verifier quon ne le fait qu'une fois par tour car si on active cette fonction, on fait indifiniment son action associ√©
+void roulement(t_joueur* tab_j,int i,BITMAP* page,int *quelle_attaque,t_map carte,int zoneAttaque[20][16],BITMAP* animation,int nb_joueur,BITMAP* soldat, int respiration)
 {
-    /*permet pendant un tour (le tour suivant) d'augmenter les PM du tank ‡ 4*/
+    /*permet pendant un tour (le tour suivant) d'augmenter les PM du tank √† 4*/
 
     int PA=4; //cout en point d'action
-
     if (action_possible(tab_j,i,PA))
     {
         *quelle_attaque=0;
         tab_j[i].PM_roule=true;
         tab_j[i].pa_actuel-=PA;
-        affichage_degat_soin(tab_j,i,page,2,soldat,carte,nb_joueur,3);
+        affichage_degat_soin(tab_j,i,page,2,soldat,carte,nb_joueur,3, respiration);
     }
     else
     {
@@ -58,9 +54,8 @@ void roulement(t_joueur* tab_j,int i,BITMAP* page,int *quelle_attaque,t_map cart
     }
 }
 
-/**  finie sans animation **/
-
-void bouclier(t_joueur* tab_j,int i,BITMAP* page,int *quelle_attaque,t_map carte,int zoneAttaque[20][16],BITMAP* animation,int nb_joueur,BITMAP* soldat)
+/** pas finie **/ //verifier quon ne le fait qu'une fois par tour car si on active cette fonction, on fait indifiniment son action associ√©
+void bouclier(t_joueur* tab_j,int i,BITMAP* page,int *quelle_attaque,t_map carte,int zoneAttaque[20][16],BITMAP* animation,int nb_joueur,BITMAP* soldat, int respiration)
 {
     int PA=6; //cout en point d'action
 
@@ -69,7 +64,7 @@ void bouclier(t_joueur* tab_j,int i,BITMAP* page,int *quelle_attaque,t_map carte
         *quelle_attaque=0;
         tab_j[i].bouclier=true;
         tab_j[i].pa_actuel-=PA;
-        affichage_degat_soin(tab_j,i,page,1,soldat,carte,nb_joueur,4);
+        affichage_degat_soin(tab_j,i,page,1,soldat,carte,nb_joueur,4, respiration);
     }
     else
     {
@@ -86,7 +81,7 @@ void tir_basique_tank(t_joueur* tab_j,int i,BITMAP* buffer,t_map carte,int zoneA
     int PA=4+1; //cout en point d'action
     int degat=200; //PV enlever a ladversaire
     int distance=3; //distance d'attaque
-    int j=joueur_sur_case_ou_pas(carte,zoneAttaque,tab_j,buffer,animation,nb_joueur); //indice du joueur attaquÈ (4 si ce n'est personne)
+    int j=joueur_sur_case_ou_pas(carte,zoneAttaque,tab_j,buffer,animation,nb_joueur); //indice du joueur attaqu√© (4 si ce n'est personne)
 
     if (action_possible(tab_j,i,PA))
     {
@@ -97,7 +92,7 @@ void tir_basique_tank(t_joueur* tab_j,int i,BITMAP* buffer,t_map carte,int zoneA
         {
             if(!(tab_j[j].bouclier)) //si le joueur ne se protege pas avec un bouclier
             {
-                tab_j[j].pv_actuel-=degat; //retire les pv au joueur attaquÈ
+                tab_j[j].pv_actuel-=degat; //retire les pv au joueur attaqu√©
                 tab_j[i].pa_actuel-=PA; //retire les PA au joueur qui joue
                 affichage_degat_soin(tab_j,j,buffer,degat,soldat,carte,nb_joueur,1);
             }
@@ -122,7 +117,7 @@ void lance_flammes(t_joueur* tab_j,int i,BITMAP* buffer,t_map carte,int zoneAtta
     int PA=2+1; //cout en point d'action
     int degat=60; //PV enlever a ladversaire
     int distance=4; //distance dattaque
-    int j=joueur_sur_case_ou_pas(carte,zoneAttaque,tab_j,buffer,animation,nb_joueur); //indice du joueur attaquÈ (4 si ce n'est personne)
+    int j=joueur_sur_case_ou_pas(carte,zoneAttaque,tab_j,buffer,animation,nb_joueur); //indice du joueur attaqu√© (4 si ce n'est personne)
 
     if (action_possible(tab_j,i,PA))
     {
@@ -133,7 +128,7 @@ void lance_flammes(t_joueur* tab_j,int i,BITMAP* buffer,t_map carte,int zoneAtta
         {
             if(!(tab_j[j].bouclier)) //si le joueur ne se protege pas avec un bouclier
             {
-                tab_j[j].pv_actuel-=degat; //retire les pv au joueur attaquÈ
+                tab_j[j].pv_actuel-=degat; //retire les pv au joueur attaqu√©
                 tab_j[j].en_feu=true; //le met en feu (perds 20pv en plus par tour)
                 tab_j[i].pa_actuel-=PA; //retire les PA au joueur qui joue
                 affichage_degat_soin(tab_j,i,buffer,degat,soldat,carte,nb_joueur,1);
